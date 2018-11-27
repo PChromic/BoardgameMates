923db6d4eabcfb472745b226fa23f1ad4c3e2db7
Przemysław Chromicz <pchromicz@gmail.com>
2018-11-27 10:56:59 +0100

    Initial commit

---
+++ src/main/java/com/pchromic/boardgamemate/BoardgameMateApplication.java
@@ -0,0 +1,12 @@
+package com.pchromic.boardgamemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameMateApplication.class, args);
+	}
+}
